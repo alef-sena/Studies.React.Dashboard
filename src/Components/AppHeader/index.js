f9baee8671dddd4001baedc4c87d251974962f3a
alef-sena <alefsena@senseup.tech>
2023-08-12 21:55:42 -0300

    ✨ implements data retrieval to dashboard dynamically

--- src/Components/AppHeader/index.js
+++ src/Components/AppHeader/index.js
@@ -1,8 +1,24 @@
 /* eslint-disable react/no-unescaped-entities */
 import { BellFilled, MailOutlined } from '@ant-design/icons';
-import { Badge, Image, Space, Typography } from 'antd';
+import { Badge, Drawer, Image, List, Space, Typography } from 'antd';
+import { useEffect, useState } from 'react';
+import { getComments, getOrders } from '../../API';
 
 function AppHeader() {
+
+	const [ comments, setComments ] = useState([]);
+	const [ orders, setOrders ] = useState([]);
+	const [ commentsOpen, setCommentsOpen ] = useState(false);
+	const [ notificationsOpen, setNotificationsOpen ] = useState(false);
+
+	useEffect(() => {
+		getComments().then(res => {
+			setComments(res.comments);
+		}, []);
+		getOrders().then(res => {
+			setOrders(res.products);
+		}, []);
+	}, []);
 	return <div className="AppHeader">
 		<Image 
 			width={40}
@@ -11,13 +27,60 @@ function AppHeader() {
 		</Image>
 		<Typography.Title>Sena's Dashboard</Typography.Title>
 		<Space>
-			<Badge count={9} dot>
-				<MailOutlined style={{ fontSize: 24}} />
+			<Badge count={comments.length} dot>
+				<MailOutlined
+					style={{ fontSize: 24}}
+					onClick={() => {
+						setCommentsOpen(true);
+					}} 
+				/>
 			</Badge>
-			<Badge count={14}>
-				<BellFilled style={{ fontSize: 24}} />
+			<Badge count={orders.length}>
+				<BellFilled
+					style={{ fontSize: 24}}
+					onClick={() => {
+						setNotificationsOpen(true);
+					}}
+				/>
 			</Badge>
 		</Space>
+		<Drawer
+			title='Comments'
+			open={commentsOpen}
+			onClose={() => {
+				setCommentsOpen(false);
+			}}
+			maskClosable
+		>
+			<List
+				dataSource={comments}
+				renderItem={(item) => {
+					return <List.Item>{item.body}</List.Item>;
+				}}
+			>
+			</List>
+		</Drawer>
+		<Drawer
+			title='Notifications'
+			open={notificationsOpen}
+			onClose={() => {
+				setNotificationsOpen(false);
+			}}
+			maskClosable
+		>
+			<List
+				dataSource={orders}
+				renderItem={(item) => {
+					return (
+						<List.Item>
+							<Typography.Text strong>{item.title}</Typography.Text>{' '}
+							has been ordered!
+						</List.Item>
+					);
+				}}
+			>
+			</List>
+		</Drawer>
 	</div>;
 }
 
